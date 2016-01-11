50af24b719a1524747d51fff689a52db0ff89266
Rodrigo de Souza Reis <rodrigo.reis@netshoes.com>
2016-01-11 14:07:32 -0200

    Initial Commit

---
+++ ios-binners-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-binners-project
+//
+//  Created by Rodrigo de Souza Reis on 11/01/16.
+//  Copyright © 2016 Rodrigo de Souza Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
