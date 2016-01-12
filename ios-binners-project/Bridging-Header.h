04eb048d6e9797ad3ab5de494d398dc750284274
Rodrigo de Souza Reis <rodrigo.reis@netshoes.com>
2016-01-12 10:43:53 -0200

    First App Screen

---
+++ ios-binners-project/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  ios-binners-project
+//
+//  Created by Rodrigo de Souza Reis on 11/01/16.
+//  Copyright © 2016 Rodrigo de Souza Reis. All rights reserved.
+//
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <Fabric/Fabric.h>
+#import <GoogleSignIn/GoogleSignIn.h>
