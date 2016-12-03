1866b19afd09beaa20ab36655d813ac2cc53aea9
Matheus Ruschel <matheus.ruschel@ilegra.com>
2016-12-03 16:36:08 -0200

    removing vc extension

--- ios-binners-project/UIViewController+Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIViewController+Extension.swift
-//  ios-binners-project
-//
-//  Created by Matheus Ruschel on 03/12/16.
-//  Copyright © 2016 Rodrigo de Souza Reis. All rights reserved.
-//
-
-import Foundation
